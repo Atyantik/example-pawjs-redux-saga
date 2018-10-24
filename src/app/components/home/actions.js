91eee06d9328befc487e9f736373a40b130337af
zeelshah <zeelshah@atyantik.com>
2018-10-24 16:11:20 +0530

    Design changes

---
+++ src/app/components/home/actions.js
@@ -0,0 +1,9 @@
+import { INCREMENT_COUNT, DECREMENT_COUNT } from './reducer';
+
+export const incrementCounter = () => ({
+    type: INCREMENT_COUNT,
+});
+
+export const decrementCounter = () => ({
+    type: DECREMENT_COUNT,
+});
--- src/server.js
+++ src/server.js
@@ -1,1 +1,35 @@
-export default class Server {}
+import ReduxServer from '@pawjs/redux/server';
+import createSagaMiddleware from 'redux-saga';
+import * as reducers from './app/reducers';
+import mySaga from './app/components/home/saga';
+
+
+const appInitialState = {};
+
+export default class Server {
+    constructor({addPlugin}) {
+
+        this.sagaMiddleware = createSagaMiddleware();
+        const reduxServer = new ReduxServer({addPlugin});
+        reduxServer.setReducers(reducers);
+        reduxServer.addMiddleware(this.sagaMiddleware);
+        addPlugin(reduxServer);
+    }
+
+    apply(serverHandler) {
+        serverHandler
+            .hooks
+            .reduxInitialState
+            .tapPromise("AppInitialState", async ({getInitialState, setInitialState}) => {
+                const initialState = Object.assign({}, getInitialState(), appInitialState);
+                setInitialState(initialState);
+            });
+
+        serverHandler
+            .hooks
+            .beforeAppRender
+            .tapPromise("RunSagaMiddleware", async () => {
+                return this.sagaMiddleware.run(mySaga);
+            });
+    }
+}
